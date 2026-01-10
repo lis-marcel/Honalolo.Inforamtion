8bf31fb6e829e8076d4ca7bd3028d657e1c2f028
marcel <lis.marcys@gmail.com>
2026-01-10 17:27:57 +0100

    [feat] added user roles enum

--- src/Infrastructure/Honalolo.Inforamtion.Domain/Enums/UserRole.cs
+++ src/Infrastructure/Honalolo.Inforamtion.Domain/Enums/UserRole.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace Honalolo.Inforamtion.Domain.Enums
 {
-    internal class UserRole
+    public enum UserRole
     {
+        Guest = 0,                 // Gość (users without accounts) 
+        RegisteredUser = 1,        // Użytkownik zarejestrowany
+        Partner = 2,               // Organizator / Partner lokalny
+        Moderator = 3,             // Moderator / Redaktor treści
+        Administrator = 99         // Administrator systemu
     }
 }
